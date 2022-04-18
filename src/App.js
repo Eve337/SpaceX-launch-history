2515f9d1bc9b78b87dc8302e00374af05be39237
eve337 <vesunchik1945@gmail.com>
2022-04-18 11:19:42 +0300

    SPACEX/Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.scss";
+
+const App = () => {
+  return (
+    <div className={styles.content}>
+      <div className={styles.label}>Create React App Without CRA😊</div>
+    </div>
+  );
+};
+
+export default App;
